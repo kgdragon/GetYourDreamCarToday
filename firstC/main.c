33f8984f0a194cf29ab8b609b8da37ceb278fd1e
kito <kito@kitos-MacBook-Pro.local>
2017-08-28 08:02:33 -0700

    Initial Commit

---
+++ firstC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  firstC
+//
+//  Created by kito on 8/28/17.
+//  Copyright © 2017 kito. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
