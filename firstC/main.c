99dad9064e757973319297f2ed3d1e6613993284
kito <kito@kitos-MacBook-Pro.local>
2017-11-13 17:03:45 -0800

    new file

--- firstC/main.c
+++ firstC/main.c
@@ -1,15 +1,15 @@
-//
-//  main.c
-//  firstC
-//
-//  Created by kito on 8/28/17.
-//  Copyright © 2017 kito. All rights reserved.
-//
 
 #include <stdio.h>
+#include <ctype.h>
+#include <time.h>
+#include<stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+double triple(int n);
+int main() {
+
+ 
+}
+
+double triple(int n) {
+    Return (3.14 * 2 * n);
 }
